ffc79437d33b0ec14188213f3bc53327f84cc55d
DDevil NightRaid <ddevil@gmail.com>
2024-05-08 15:49:48 +0545

    notification test

--- src/app/api/(intervals)/thirtyinterval/route.ts
+++ src/app/api/(intervals)/thirtyinterval/route.ts
@@ -1,4 +1,12 @@
 import { NextResponse } from 'next/server';
 export function GET() {
+    const notification = new Notification("Water Time", {
+        body: "Time to drink some water!",
+        icon: "/assets/items/drinks/glass-of-water.svg",
+        vibrate: [200, 100, 200, 100, 200, 100, 200],
+        tag: "water-notification",
+        renotify: true,
+        silent: false,
+    })
     return new NextResponse(JSON.stringify({"message": "it works"}))    
 }
